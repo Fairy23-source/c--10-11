d971380bc5744d26d24c87b7171c373ffd4c626b
Fairy Solanki <solankifairy612@gmail.com>
2026-01-17 15:40:34 +0530

    Add files via upload

---
+++ 10b.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+int i=1;
+do{
+    
+    cout<<i<<endl;
+    i++;
+    
+}while(i<100);
+return 0;
+}
+//syntax is same as c we can use for loop to print more no of times 
