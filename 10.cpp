36c9ccb1293e82527ec7cbd557b27d887c339174
Fairy Solanki <solankifairy612@gmail.com>
2026-01-17 15:32:22 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+int i;
+for(i=0;i<100;i++)
+cout<<i<<endl;
+return 0;
+}
+//syntax is same as c we can use for loop to print more no of times 
